65bc67f775adaee87cb3fec872916b3ff3aada4f
raisayon <59853083+raisayon@users.noreply.github.com>
2026-03-10 17:22:43 +0800

    Update learningpuppet.py

--- ConfigurationManagement/learningpuppet.py
+++ ConfigurationManagement/learningpuppet.py
@@ -28,3 +28,18 @@ class timezone {
       }
 
 }
+
+#This code block includes a class with three resources, a package, a file, and a service. All of them are related to the Network Time Protocol, or NTP, the mechanism our computers use to synchronize the clocks. This code ensures that the NTP package is always upgraded to the latest version. We're setting the contents of the configuration file using the source attribute, which means that the agent will read the required contents from the specified location. And we're saying that we want the NTP service to be enabled and running. By grouping all of the resources related to NTP in the same class, we only need a quick glance to understand how the service is configured and how it's supposed to work. This would make it easier to make changes in the future since we have all the related resources together. It makes sense to use this technique whenever we want to group related resources. 
+class ntp {
+  package { 'ntp':
+    ensure => latest,
+  }
+  file { '/etc/ntp.conf':
+    source => 'puppet:///modules/ntp/ntp.conf'
+    replace => true,
+  }
+  service { 'ntp':
+    enable  => true,
+    ensure  => running,
+  }
+}
