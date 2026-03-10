dc9ef5314ba5cbed33890389f79edbbc3e0f3383
raisayon <59853083+raisayon@users.noreply.github.com>
2026-03-10 17:46:07 +0800

    Update learningpuppet.py

--- ConfigurationManagement/learningpuppet.py
+++ ConfigurationManagement/learningpuppet.py
@@ -61,3 +61,21 @@ class AutoConfig {
     ensure  => running,
   }
 }
+
+# This code block is a Puppet manifest, which is a configuration file used to manage systems using the Puppet automation framework.
+# The code you provided is an if statement. An if statement is a conditional statement that executes a block of code if a certain condition is met. In this case, the condition is whether the is_virtual fact is set to true. The is_virtual fact is a built-in fact that Puppet uses to determine if the node is a virtual machine.
+# If the is_virtual fact is set to true, then the code in the if statement block will be executed. This code will purge the smartmontools package. The smartmontools package is a software package that provides tools for monitoring and managing hard drives. Purging the smartmontools package on a virtual machine is typically done to improve performance.
+
+# If the is_virtual fact is set to false, then the code in the else statement block will be executed. This code will install the smartmontools package.
+
+# In this code block, the value of the is_virtual fact is true, so the code in the if statement block will be executed. This means that the smartmontools package will be purged.
+if $facts['is_virtual'] {
+  package { 'smartmontools':
+    ensure => purged,
+
+  }
+} else {
+  package { 'smartmontools':
+    ensure => installed,
+  }
+}
