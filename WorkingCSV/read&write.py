d860fbebb575816269f1d1569620282713205763
raisayon <59853083+raisayon@users.noreply.github.com>
2026-07-29 12:53:48 +0800

    Modify contents_of_file to read CSV without dictionary
    Updated the contents_of_file function to process CSV data without using a dictionary and added functionality to skip the header record.

--- WorkingCSV/read&write.py
+++ WorkingCSV/read&write.py
@@ -34,3 +34,40 @@ def contents_of_file(filename):
 
 #Call the function
 print(contents_of_file("flowers.csv"))
+
+#Question 2 Using the CSV file of flowers again, fill in the gaps of the contents_of_file function to process the data without turning it into a dictionary. How do you skip over the header record with the field names?
+
+import os
+import csv
+
+# Create a file with data in it
+def create_file(filename):
+  with open(filename, "w") as file:
+    file.write("name,color,type\n")
+    file.write("carnation,pink,annual\n")
+    file.write("daffodil,yellow,perennial\n")
+    file.write("iris,blue,perennial\n")
+    file.write("poinsettia,red,perennial\n")
+    file.write("sunflower,yellow,annual\n")
+
+# Read the file contents and format the information about each row
+def contents_of_file(filename):
+  return_string = ""
+
+  # Call the function to create the file 
+  create_file(filename)
+
+  # Open the file
+  with open(filename,"r") as f:
+    # Read the rows of the file
+    rows = csv.DictReader(f)
+    # Process each row
+    for row in rows:
+      name,color,type = row["name"],row["color"],row["type"]
+      # Format the return string for data rows only
+
+      return_string += "a {} {} is {}\n".format(color,name,type)
+  return return_string
+
+#Call the function
+print(contents_of_file("flowers.csv"))
